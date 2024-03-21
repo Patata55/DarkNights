95ce53e8e5287a26645b55bfa589bb674a1bdc24
Donuttective <Donuttective@gmail.com>
2024-03-21 10:20:45 -0500

    Scrip disparopro
    scrip

--- Assets/Tonio/TScripts/Disparo pro.cs
+++ Assets/Tonio/TScripts/Disparo pro.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Disparopro : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject bala;
+    public Transform Aparicionepica;
+
+    public float fuercadisparo = 1500;
+    public float rangodisparo = 0.5f;
+
+    private float rangodisparotiempo = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Time.time>rangodisparotiempo) 
+            {
+                GameObject nuebavala;
+                nuebavala = Instantiate(bala,Aparicionepica.position,Aparicionepica.rotation);
+                nuebavala.GetComponent<Rigidbody>().AddForce(Aparicionepica.forward)
+
+            }
+        }
     }
 }
