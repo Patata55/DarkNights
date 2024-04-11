9d69ea5a1b9dfdf621844390e5c0c58d896fe066
Donuttective <Donuttective@gmail.com>
2024-04-11 09:45:16 -0500

    Revert "Scrip disparopro"
    This reverts commit 95ce53e8e5287a26645b55bfa589bb674a1bdc24.

--- Assets/Tonio/TScripts/Disparo pro.cs
+++ Assets/Tonio/TScripts/Disparo pro.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class Disparopro : MonoBehaviour
 {
-    public GameObject bala;
-    public Transform Aparicionepica;
-
-    public float fuercadisparo = 1500;
-    public float rangodisparo = 0.5f;
-
-    private float rangodisparotiempo = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            if (Time.time>rangodisparotiempo) 
-            {
-                GameObject nuebavala;
-                nuebavala = Instantiate(bala,Aparicionepica.position,Aparicionepica.rotation);
-                nuebavala.GetComponent<Rigidbody>().AddForce(Aparicionepica.forward)
-
-            }
-        }
+        
     }
 }
